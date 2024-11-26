1a8ac34b2320adcc0e01cf3e36080614a3c44938
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2024-11-26 22:03:56 +0400

    Update HomePage.tsx
    Updated layout and mobile view

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -13,9 +13,9 @@ const HomePage = () => {
                 <div className="w-full flex flex-row flex-wrap lg:flex-nowrap">
                     <div className="w-full lg:w-2/3 p-4 lg:border-r border-gray-800">
                         {/* Usage box */}
-                        <div className="p-4 mb-2 border-b border-gray-800 flex flex-row flex-nowrap">
-                            <div className="w-2/3">
-                                <div className="text-3xl flex flex-row flex-wrap mb-2">
+                        <div className="p-4 mb-2 border-b border-gray-800 flex flex-row flex-wrap justify-evenly">
+                            <div className="w-fit">
+                                <div className="text-3xl flex flex-row flex-wrap mb-4">
                                     <h2 className="mr-2">Current usage:</h2>
                                     <h2 className="font-bold">1375 w</h2>
                                 </div>
@@ -32,17 +32,15 @@ const HomePage = () => {
                                     <h2 className="font-bold">990 kWh</h2>
                                 </div>
                             </div>
-                            <div className="w-1/3 text-xl flex flex-wrap items-center justify-center">
+                            <div className="w-fit mt-4 text-xl flex flex-nowrap items-center lg:justify-center justify-start">
                                 <h2 className="mr-2">Estimated:</h2>
                                 <h2 className="font-bold">350$</h2>
                             </div>
                         </div>
                         {/* Optimisation box */}
                         <div className="py-4 px-2 flex flex-col justify-center">
-                            <div className="text-xl flex flex-row flex-wrap">
-                                <h2 className="font-bold mb-2">
-                                    AI Recommendations
-                                </h2>
+                            <div className="text-xl text-center mb-2">
+                                <h2>AI Recommendations</h2>
                             </div>
 
                             <div className="p-4 m-2 bg-gray-700 rounded-md hover:cursor-pointer">
@@ -87,54 +85,54 @@ const HomePage = () => {
                             </h2>
                         </div>
 
-                        <div className="w-full flex flex-row justify-evenly">
-                            <div className="min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
+                        <div className="w-full flex flex-row flex-wrap justify-evenly">
+                            <div className="mt-4 min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
                                 <FontAwesomeIcon size="2x" icon={faFire} />
                                 <h2 className="ml-2 text-lg">Heating</h2>
                             </div>
-                            <div className="min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
+                            <div className="mt-4 min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
                                 <FontAwesomeIcon size="2x" icon={faSnowflake} />
                                 <h2 className="ml-2 text-lg">Cooling</h2>
                             </div>
-                            <div className="min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
+                            <div className="mt-4 min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
                                 <FontAwesomeIcon size="2x" icon={faWind} />
                                 <h2 className="ml-2 text-lg">Ventilation</h2>
                             </div>
                         </div>
 
-                        <div className="w-full mt-6 flex flex-row justify-evenly">
-                            <div className="min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
+                        <div className="w-full mt-2 flex flex-row flex-wrap justify-evenly">
+                            <div className="mt-4 min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
                                 <FontAwesomeIcon size="2x" icon={faWind} />
                                 <h2 className="ml-2 text-lg">Ventilation</h2>
                             </div>
-                            <div className="min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
+                            <div className="mt-4 min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
                                 <FontAwesomeIcon size="2x" icon={faFire} />
                                 <h2 className="ml-2 text-lg">Heating</h2>
                             </div>
-                            <div className="min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
+                            <div className="mt-4 min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
                                 <FontAwesomeIcon size="2x" icon={faSnowflake} />
                                 <h2 className="ml-2 text-lg">Cooling</h2>
                             </div>
                         </div>
 
-                        <div className="w-full mt-6 flex flex-row justify-evenly">
-                            <div className="min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
+                        <div className="w-full mt-2 flex flex-row flex-wrap justify-evenly">
+                            <div className="mt-4 min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
                                 <FontAwesomeIcon size="2x" icon={faSnowflake} />
                                 <h2 className="ml-2 text-lg">Cooling</h2>
                             </div>
-                            <div className="min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
+                            <div className="mt-4 min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
                                 <FontAwesomeIcon size="2x" icon={faWind} />
                                 <h2 className="ml-2 text-lg">Ventilation</h2>
                             </div>
-                            <div className="min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
+                            <div className="mt-4 min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
                                 <FontAwesomeIcon size="2x" icon={faFire} />
                                 <h2 className="ml-2 text-lg">Heating</h2>
                             </div>
                         </div>
 
-                        <div className="w-full mt-6 flex flex-row justify-evenly">
-                            <div className="min-w-1/5 max-w-2/5 w-fit bg-gray-700 rounded p-4 flex flex-row justify-center items-center">
-                                <FontAwesomeIcon icon={faPlus} />
+                        <div className="w-full mt-6 flex flex-row flex-wrap justify-evenly">
+                            <div className="mt-4 min-w-1/5 max-w-2/5 w-fit bg-gray-800 rounded p-4 flex flex-row justify-center items-center">
+                                <FontAwesomeIcon size="2x" icon={faPlus} />
                                 <h2 className="ml-2 text-lg">Add more</h2>
                             </div>
                         </div>
