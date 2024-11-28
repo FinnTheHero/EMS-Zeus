e11f5accc2f6b5ae7410bbcf417690b932331c3a
FinnTheHero <aleksandre.nozadze.1@iliauni.edu.ge>
2024-11-28 15:34:50 +0400

    Update TeamPage.tsx
    Fixed typo

--- src/Pages/TeamPage.tsx
+++ src/Pages/TeamPage.tsx
@@ -2,7 +2,7 @@ const TeamPage = () => {
   return (
     <div className="w-full flex justify-center items-center">
       <div className="text-2xl max-w-5xl flex flex-col items-center justify-center">
-        <h2 className="text-3xl mb-4">Contat us</h2>
+        <h2 className="text-3xl mb-4">Contact us</h2>
         <div className="text-center">
           <h2>Aleksandre Nozadze</h2>
           <h2>aleksandre.nozadze.1@iliauni.edu.ge</h2>
